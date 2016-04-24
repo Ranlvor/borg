b2a50c3df75090855076f288303ca07bcb21ad9c
Ranlvor <ranlvor@starletp9.de>
2016-04-24 12:49:55 +0200

    first try with travis

--- .travis/upload_coverage.sh
+++ .travis/upload_coverage.sh
@@ -8,6 +8,8 @@ if ! [[ "${NO_COVERAGE_TOXENVS[*]}" =~ "${TOXENV}" ]]; then
     source ~/.venv/bin/activate
     ln .tox/.coverage .coverage
     # on osx, tests run as root, need access to .coverage
-    sudo chmod 666 .coverage
+    if [ ! -r .coverage ]; then
+        sudo chmod 666 .coverage
+    fi
     codecov -e TRAVIS_OS_NAME TOXENV
 fi
